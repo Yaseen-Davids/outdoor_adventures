43e18ac04bc336dc4ae59b8799670c725d4a3180
Yaseen Davids <yaseendavids477@gmail.com>
2018-05-14 11:58:39 +0200

    navbar and home image added

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// function checkHeight() {
+//     var currentScrollPos = document.documentElement.scrollTop;
+//     var navHead = document.getElementById("nav_header");
+
+//     if (currentScrollPos < 750) {
+//         navHead.style.display = "none";
+//         navHead.style.opacity = "0";
+
+//     }
+//     else if (currentScrollPos > 750) {
+//         navHead.style.display = "block";
+//         navHead.style.opacity = "1";
+//     }
+// }
