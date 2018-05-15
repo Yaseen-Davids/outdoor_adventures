48b3c1369c1d2741cfaa486e59311a6a4872cf43
Yaseen Davids <yaseendavids477@gmail.com>
2018-05-15 11:56:20 +0200

    Hiking Category Images, Header and Info Added. Javascript Function also added 100%

--- js/script.js
+++ js/script.js
@@ -12,3 +12,80 @@
 //         navHead.style.opacity = "1";
 //     }
 // }
+
+function showLionInfo() {
+    let boxInfo = document.getElementById("infobox");
+    let info1Show = document.getElementById("lionshead");
+    let info2Show = document.getElementById("skeleton");
+    let info3Show = document.getElementById("motte");
+
+    if (info2Show.style.display === "block"){
+        info2Show.style.display = "none";
+    }
+
+    if (info3Show.style.display === "block"){
+        info3Show.style.display = "none";
+    }
+
+    if (boxInfo.style.display === "block" && info1Show.style.display === 'block'){
+        boxInfo.style.display = "none";
+        info1Show.style.display = "none";
+    }
+
+    else {
+        boxInfo.style.display = "block";
+        info1Show.style.display = "block";
+    }
+}
+
+function showSkeletonInfo() {
+    let boxInfo = document.getElementById("infobox");
+    let info1Show = document.getElementById("lionshead");
+    let info2Show = document.getElementById("skeleton");
+    let info3Show = document.getElementById("motte");
+
+    if (info1Show.style.display === "block"){
+        info1Show.style.display = "none";
+    }
+
+    if (info3Show.style.display === "block"){
+        info3Show.style.display = "none";
+    }
+
+    if (boxInfo.style.display === "block" && info2Show.style.display === 'block'){
+        boxInfo.style.display = "none";
+        info2Show.style.display = "none";
+    } 
+        else {
+        boxInfo.style.display = "block";
+        info2Show.style.display = "block";
+    }
+}
+
+function showMotteInfo() {
+    let boxInfo = document.getElementById("infobox");
+    let info1Show = document.getElementById("lionshead");
+    let info2Show = document.getElementById("skeleton");
+    let info3Show = document.getElementById("motte");
+
+    if (info1Show.style.display === "block"){
+        info1Show.style.display = "none";
+    }
+
+    if (info2Show.style.display === "block"){
+        info2Show.style.display = "none";
+    }
+
+    if (info2Show.style.display === "block"){
+        info2Show.style.display = "none";
+    }
+
+    if (boxInfo.style.display === "block" && info3Show.style.display === 'block'){
+        boxInfo.style.display = "none";
+        info3Show.style.display = "none";
+    } 
+        else {
+        boxInfo.style.display = "block";
+        info3Show.style.display = "block";
+    }
+}
